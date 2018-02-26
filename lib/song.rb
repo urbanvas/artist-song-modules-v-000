93ea1dba937ac9da3a40877ddbbf521c914d56b4
Neil Seejoor <nvseejoor@gmail.com>
2018-02-26 03:45:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ class Song
 
   extend Memorable
   extend Findable
-  
+
   attr_accessor :name
   attr_reader :artist
 
@@ -16,10 +16,6 @@ class Song
 
   def artist=(artist)
     @artist = artist
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
 
   def self.all
